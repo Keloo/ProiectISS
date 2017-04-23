872fbde4dc5fe921a8daa04328c185a08dcdfb45
Keloo <vlad-negura@mail.ru>
2017-04-23 12:34:12 +0300

    adding more fixtures, adjusting db schema

--- src/AppBundle/DataFixtures/ORM/LoadConferenceData.php
+++ src/AppBundle/DataFixtures/ORM/LoadConferenceData.php
@@ -11,15 +11,40 @@ class LoadConferenceData extends AbstractFixture implements OrderedFixtureInterf
 {
     public function load(ObjectManager $manager)
     {
-        $conference = new Conference();
-        $conference->setName('Conference_name 1');
+        $startTime = new \DateTime('now');
+        $startTime->modify('-1 year');
+        $endTime = new \DateTime('now');
+        $endTime->modify('-10 months');
 
-        $manager->persist($conference);
+        $conference1 = new Conference();
+        $conference1
+            ->setName('Conference_name 1')
+            ->setStartTime($startTime)
+            ->setCallForPapersStart($startTime)
+            ->setPaperSubmissionStart($startTime)
+            ->setEndTime($endTime)
+            ->setCallForPapersEnd($endTime)
+            ->setPaperSubmissionEnd($endTime);
+        $manager->persist($conference1);
         $manager->flush();
+        $this->addReference('conference1', $conference1);
+
+        $conference2 = new Conference();
+        $conference2
+            ->setName('Conference_name 2')
+            ->setStartTime($startTime->modify('+5 months'))
+            ->setCallForPapersStart($startTime->modify('+5 months'))
+            ->setPaperSubmissionStart($startTime->modify('+5 months'))
+            ->setEndTime($endTime->modify('+5 months'))
+            ->setCallForPapersEnd($endTime->modify('+5 months'))
+            ->setPaperSubmissionEnd($endTime->modify('+5 months'));
+        $manager->persist($conference2);
+        $manager->flush();
+        $this->addReference('conference2', $conference2);
     }
 
     public function getOrder()
     {
-        return 2;
+        return 1;
     }
 }
--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -12,20 +12,85 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
     public function load(ObjectManager $manager)
     {
         $userAdmin = new User();
-        $userAdmin->setUsername('admin');
-        $userAdmin->setPassword('admin');
-        $userAdmin->setRoles(['ROLE_SUPER_ADMIN']);
-        $userAdmin->setFirstName('Admin');
-        $userAdmin->setLastName('Super');
-
+        $userAdmin
+            ->setUsername('admin')
+            ->setEmail('admin@example.com')
+            ->setPassword('admin')
+            ->setFirstName('Super')
+            ->setLastName('Admin')
+            ->setRoles(['ROLE_SUPER_ADMIN']);
         $manager->persist($userAdmin);
         $manager->flush();
-
         $this->addReference('admin-user', $userAdmin);
+
+        $user = new User();
+        $user
+            ->setUsername('user')
+            ->setEmail('user@example.com')
+            ->setFirstName('user_first')
+            ->setLastName('user_last')
+            ->setPassword('user')
+            ->setRoles(['ROLE_USER'])
+            ->addConference($this->getReference('conference1'));
+        $manager->persist($user);
+        $manager->flush();
+        $this->addReference('user-user', $user);
+
+        $reviewer = new User();
+        $reviewer
+            ->setUsername('reviewer')
+            ->setEmail('reviewer@example.com')
+            ->setPassword('reviewer')
+            ->setFirstName('reviewer_first')
+            ->setLastName('reviewer_last')
+            ->setRoles(['ROLE_REVIEWER'])
+            ->addConference($this->getReference('conference1'));
+        $manager->persist($reviewer);
+        $manager->flush();
+        $this->addReference('reviewer-user', $reviewer);
+
+        $listener = new User();
+        $listener
+            ->setUsername('listener')
+            ->setEmail('listener@example.com')
+            ->setPassword('listener')
+            ->setFirstName('listener_first')
+            ->setLastName('listener_last')
+            ->setRoles(['ROLE_LISTENER'])
+            ->addConference($this->getReference('conference1'));
+        $manager->persist($listener);
+        $manager->flush();
+        $this->addReference('listener-user', $listener);
+
+        $speaker = new User();
+        $speaker
+            ->setUsername('speaker')
+            ->setEmail('speaker@example.com')
+            ->setPassword('speaker')
+            ->setFirstName('speaker_first')
+            ->setLastName('speaker_last')
+            ->setRoles(['ROLE_SPEAKER'])
+            ->addConference($this->getReference('conference1'));
+        $manager->persist($speaker);
+        $manager->flush();
+        $this->addReference('speaker-user', $speaker);
+
+        $pc = new User();
+        $pc
+            ->setUsername('pc')
+            ->setEmail('pc@example.com')
+            ->setPassword('pc')
+            ->setFirstName('pc_first')
+            ->setLastName('pc_last')
+            ->setRoles(['ROLE_PC'])
+            ->addConference($this->getReference('conference1'));
+        $manager->persist($pc);
+        $manager->flush();
+        $this->addReference('pc-user', $pc);
     }
 
     public function getOrder()
     {
-        return 1;
+        return 2;
     }
 }
